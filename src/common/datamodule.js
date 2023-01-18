a3fb9b0751fc7b2dc7cdecdda5713ade7c589c01
alejozuleta9217 <jazuletam@dane.gov.co>
2023-01-18 14:17:15 -0500

    first commit

--- src/common/datamodule.js
+++ src/common/datamodule.js
@@ -2,21 +2,18 @@ export default class MapFile{
   
     static fileregistrer (data){
       let location = [];
-      if(typeof data.length === "undefined" ){
-        location[0] =
-        {
-          ID: 1,
-          FirstName: 'Sin Datos',
+        if(data === undefined){
+          return location;
+        }else{
+          for (let i = 0; i < data.length; i++) {
+            location[i] =
+               {
+                  ID: data[i].numero_modulo,
+                  FirstName: data[i].estado_modulo,
+               }          
+           }
         }
-      }else{
-        for (let i = 0; i < data.length; i++) {
-          location[i] =
-             {
-                ID: data[i].numero_modulo,
-                FirstName: data[i].estado_modulo,
-             }          
-         }
-      }
+      
       return location;
     }  
   
